db931ca158e35bbaac18fc05b1f78df4be141da1
VNWA <nhatnguyen061003@gmail.com>
2024-12-10 06:47:39 +0700

    ss3

--- database/migrations/2024_10_16_100735_create_products_table.php
+++ database/migrations/2024_10_16_100735_create_products_table.php
@@ -14,32 +14,33 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->tinyInteger('is_show')->default(1);
-            $table->tinyInteger('is_seller')->default( 0);
+            $table->tinyInteger('is_seller')->default(0);
             $table->json('images');
             $table->foreignId('brand_id')->nullable()->constrained('brands')->onDelete('set null');
             $table->foreignId('color_id')->nullable()->constrained('colors')->onDelete('set null');
-            $table->string('sku', 191)->unique();
+            $table->string('sku', 191)->unique(); // Chuyển sang VARCHAR với độ dài 191
             $table->string('size')->nullable();
             $table->string('included')->nullable();
             $table->integer('stock')->default(0);
             $table->tinyInteger('availability')->default(1);
             $table->string('origin');
             $table->string('ean')->nullable();
-            $table->text('name');
-            $table->text('slug')->unique();
+            $table->string('name'); // Chuyển từ text sang string
+            $table->string('slug', 191)->unique(); // Chuyển từ TEXT sang VARCHAR với độ dài 191
             $table->integer('price');
-            $table->tinyInteger('is_discount')->default( 0);
-            $table->enum('discount_type', ['percentage', 'amount'])->nullable(); // Sử dụng enum cho discount_type
+            $table->tinyInteger('is_discount')->default(0);
+            $table->enum('discount_type', ['percentage', 'amount'])->nullable();
             $table->integer('discount_price')->nullable();
             $table->longText('description')->nullable();
             $table->longText('ingredients')->nullable();
             $table->longText('how_to_use')->nullable();
-            $table->text('meta_image')->nullable();
-            $table->text('meta_title')->nullable();
-            $table->text('meta_desc')->nullable();
-            $table->fullText(['name', 'slug', 'sku']);
+            $table->string('meta_image')->nullable(); // Chuyển từ text sang string
+            $table->string('meta_title')->nullable(); // Chuyển từ text sang string
+            $table->string('meta_desc')->nullable(); // Chuyển từ text sang string
+            $table->fullText(['name', 'slug', 'sku']); // Full-text search vẫn giữ nguyên
             $table->timestamps();
         });
+
     }
 
     /**
